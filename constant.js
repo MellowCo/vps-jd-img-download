d42e273ab6b42972c57ee7bebdfade12a9588f91
mellow <799478052@qq.com>
2021-05-29 16:32:18 +0800

    feat: init

---
+++ constant.js
@@ -0,0 +1,7 @@
+export const DETAIL = '#product-detail-1 > div > div > img'
+export const LIST = '#spec-list > div > ul > li > img'
+
+export const IMG_TYPE = {
+  LIST: 'list',
+  DETAIL: 'detail'
+}
